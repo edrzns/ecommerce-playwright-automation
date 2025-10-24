8f671fe20aaf5a4c0df32e72fda0cefac57a30c6
CERZ <edijs.rozens@bite.lv>
2025-10-24 14:58:41 +0300

    Complete base draft of register user test

--- tests/account/register-user.spec.ts
+++ tests/account/register-user.spec.ts
@@ -2,17 +2,30 @@ import { test, expect } from "@playwright/test";
 import { faker } from "@faker-js/faker";
 
 test.describe("User registration", () => {
-  test("should register a new user successfully and show confirmation message", async ({ page }) => {
+  test("should register a new user successfully and show confirmation message", async ({
+    page,
+  }) => {
+    const randomSex = faker.person.sex();
     const randomFirstName = faker.person.firstName();
     const randomLastName = faker.person.lastName();
+    const randomBirthDate = faker.date.birthdate({
+      mode: "age",
+      min: 18,
+      max: 65,
+    });
+    const day = randomBirthDate.getDate().toString();
+    const month = (randomBirthDate.getMonth() + 1).toString();
+    const year = randomBirthDate.getFullYear().toString();
+
     const randomFullName = `${randomFirstName} ${randomLastName}`;
     const randomEmail = `${randomFirstName}.${randomLastName}_${Date.now()}@example.com`;
     const randomPassword = faker.internet.password();
     const randomCompany = faker.company.name();
+    const randomCity = faker.location.city();
     const randomAddress = faker.location.streetAddress();
     const randomCountryState = faker.location.state();
     const randomZipCode = faker.location.zipCode();
-    const randomPhoneNumber = faker.phone.number({ style: 'international' });
+    const randomPhoneNumber = faker.phone.number({ style: "international" });
 
     await page.goto("/");
     await page.getByRole("button", { name: "Consent" }).click();
@@ -24,13 +37,25 @@ test.describe("User registration", () => {
       .getByPlaceholder("Email Address")
       .fill(randomEmail);
     await page.getByRole("button", { name: "Signup" }).click();
-    await page.getByRole("radio", { name: "Mr." }).check(); // TODO: Choose depending on gender from faker-js
-    await page.getByRole("textbox", { name: "Password *" }).fill(randomPassword);
-    await page.locator("#days").selectOption("9"); // TODO: Generate birth date with faker-js
-    await page.locator("#months").selectOption("5"); //
-    await page.locator("#years").selectOption("1992"); //
-    await page.getByRole("textbox", { name: "First name *" }).fill(randomFirstName);
-    await page.getByRole("textbox", { name: "Last name *" }).fill(randomLastName);
+
+    if (randomSex === "male") {
+      await page.getByRole("radio", { name: "Mr." }).check();
+    } else {
+      await page.getByRole("radio", { name: "Mrs." }).check();
+    }
+
+    await page
+      .getByRole("textbox", { name: "Password *" })
+      .fill(randomPassword);
+    await page.locator("#days").selectOption(day);
+    await page.locator("#months").selectOption(month);
+    await page.locator("#years").selectOption(year);
+    await page
+      .getByRole("textbox", { name: "First name *" })
+      .fill(randomFirstName);
+    await page
+      .getByRole("textbox", { name: "Last name *" })
+      .fill(randomLastName);
     await page
       .getByRole("textbox", { name: "Company", exact: true })
       .fill(randomCompany);
@@ -38,10 +63,12 @@ test.describe("User registration", () => {
       .getByRole("textbox", { name: "Address * (Street address, P." })
       .fill(randomAddress);
     await page.getByLabel("Country *").selectOption("Canada");
-    await page.getByRole("textbox", { name: "State *" }).fill(randomCountryState);
+    await page
+      .getByRole("textbox", { name: "State *" })
+      .fill(randomCountryState);
     await page
       .getByRole("textbox", { name: "City * Zipcode *" })
-      .fill("The City"); // TODO: Fill City from faker-js
+      .fill(randomCity);
     await page.locator("#zipcode").fill(randomZipCode);
     await page
       .getByRole("textbox", { name: "Mobile Number *" })
@@ -50,6 +77,8 @@ test.describe("User registration", () => {
 
     await expect(page.getByText("Account Created!")).toBeVisible();
     await page.getByRole("link", { name: "Continue" }).click();
-    await expect(page.getByText(`Logged in as ${randomFullName}`)).toBeVisible();
+    await expect(
+      page.getByText(`Logged in as ${randomFullName}`)
+    ).toBeVisible();
   });
 });
