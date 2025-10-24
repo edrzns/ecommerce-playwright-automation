d214b7ab8b7ba8360edb0365d9a52c727e0ec4fa
CERZ <edijs.rozens@bite.lv>
2025-10-24 15:47:28 +0300

    Add logout step in register user test

--- tests/account/register-user.spec.ts
+++ tests/account/register-user.spec.ts
@@ -1,32 +1,32 @@
 import { test, expect } from "@playwright/test";
 import { faker } from "@faker-js/faker";
 
+const randomSex = faker.person.sex();
+const randomFirstName = faker.person.firstName();
+const randomLastName = faker.person.lastName();
+const randomBirthDate = faker.date.birthdate({
+  mode: "age",
+  min: 18,
+  max: 65,
+});
+const day = randomBirthDate.getDate().toString();
+const month = (randomBirthDate.getMonth() + 1).toString();
+const year = randomBirthDate.getFullYear().toString();
+
+const randomFullName = `${randomFirstName} ${randomLastName}`;
+const randomEmail = `${randomFirstName}.${randomLastName}_${Date.now()}@example.com`;
+const randomPassword = faker.internet.password();
+const randomCompany = faker.company.name();
+const randomCity = faker.location.city();
+const randomAddress = faker.location.streetAddress();
+const randomCountryState = faker.location.state();
+const randomZipCode = faker.location.zipCode();
+const randomPhoneNumber = faker.phone.number({ style: "international" });
+
 test.describe("User registration", () => {
   test("should register a new user successfully and show confirmation message", async ({
     page,
   }) => {
-    const randomSex = faker.person.sex();
-    const randomFirstName = faker.person.firstName();
-    const randomLastName = faker.person.lastName();
-    const randomBirthDate = faker.date.birthdate({
-      mode: "age",
-      min: 18,
-      max: 65,
-    });
-    const day = randomBirthDate.getDate().toString();
-    const month = (randomBirthDate.getMonth() + 1).toString();
-    const year = randomBirthDate.getFullYear().toString();
-
-    const randomFullName = `${randomFirstName} ${randomLastName}`;
-    const randomEmail = `${randomFirstName}.${randomLastName}_${Date.now()}@example.com`;
-    const randomPassword = faker.internet.password();
-    const randomCompany = faker.company.name();
-    const randomCity = faker.location.city();
-    const randomAddress = faker.location.streetAddress();
-    const randomCountryState = faker.location.state();
-    const randomZipCode = faker.location.zipCode();
-    const randomPhoneNumber = faker.phone.number({ style: "international" });
-
     await page.goto("/");
     await page.getByRole("button", { name: "Consent" }).click();
     await page.getByRole("link", { name: "Signup / Login" }).click();
@@ -80,5 +80,7 @@ test.describe("User registration", () => {
     await expect(
       page.getByText(`Logged in as ${randomFullName}`)
     ).toBeVisible();
+
+    await page.getByRole("link", { name: "Logout" }).click();
   });
 });
