7dda05dbdbd273a6b953206ca75e5dfc92100597
CERZ <edijs.rozens@bite.lv>
2025-10-24 14:42:32 +0300

    Complete fixing register user test case

--- tests/account/register-user.spec.ts
+++ tests/account/register-user.spec.ts
@@ -2,11 +2,17 @@ import { test, expect } from "@playwright/test";
 import { faker } from "@faker-js/faker";
 
 test.describe("User registration", () => {
-  test("test", async ({ page }) => {
+  test("should register a new user successfully and show confirmation message", async ({ page }) => {
     const randomFirstName = faker.person.firstName();
     const randomLastName = faker.person.lastName();
     const randomFullName = `${randomFirstName} ${randomLastName}`;
-    const randomEmail = `${faker.internet.username()}_${Date.now()}@example.com`;
+    const randomEmail = `${randomFirstName}.${randomLastName}_${Date.now()}@example.com`;
+    const randomPassword = faker.internet.password();
+    const randomCompany = faker.company.name();
+    const randomAddress = faker.location.streetAddress();
+    const randomCountryState = faker.location.state();
+    const randomZipCode = faker.location.zipCode();
+    const randomPhoneNumber = faker.phone.number({ style: 'international' });
 
     await page.goto("/");
     await page.getByRole("button", { name: "Consent" }).click();
@@ -19,7 +25,7 @@ test.describe("User registration", () => {
       .fill(randomEmail);
     await page.getByRole("button", { name: "Signup" }).click();
     await page.getByRole("radio", { name: "Mr." }).check(); // TODO: Choose depending on gender from faker-js
-    await page.getByRole("textbox", { name: "Password *" }).fill("Welcome12#");
+    await page.getByRole("textbox", { name: "Password *" }).fill(randomPassword);
     await page.locator("#days").selectOption("9"); // TODO: Generate birth date with faker-js
     await page.locator("#months").selectOption("5"); //
     await page.locator("#years").selectOption("1992"); //
@@ -27,21 +33,23 @@ test.describe("User registration", () => {
     await page.getByRole("textbox", { name: "Last name *" }).fill(randomLastName);
     await page
       .getByRole("textbox", { name: "Company", exact: true })
-      .fill("The Company"); // TODO: Fill company name from faker-js
+      .fill(randomCompany);
     await page
       .getByRole("textbox", { name: "Address * (Street address, P." })
-      .fill("The Address"); // TODO: Fill address from faker-js
-    await page.getByLabel("Country *").selectOption("Canada"); // Fill country from faker-js or select from dropdown
-    await page.getByRole("textbox", { name: "State *" }).fill("The State"); // Fill state from faker-js
+      .fill(randomAddress);
+    await page.getByLabel("Country *").selectOption("Canada");
+    await page.getByRole("textbox", { name: "State *" }).fill(randomCountryState);
     await page
       .getByRole("textbox", { name: "City * Zipcode *" })
       .fill("The City"); // TODO: Fill City from faker-js
-    await page.locator("#zipcode").fill("The Zipcode"); // TODO: Fill zipcode from faker-js
+    await page.locator("#zipcode").fill(randomZipCode);
     await page
       .getByRole("textbox", { name: "Mobile Number *" })
-      .fill("The Mobile Number"); // TODO: Fill random mobile number from faker-js
+      .fill(randomPhoneNumber);
     await page.getByRole("button", { name: "Create Account" }).click();
-    await page.getByText("Account Created!").click(); // Create assertion to expect text "Account Created!"
+
+    await expect(page.getByText("Account Created!")).toBeVisible();
     await page.getByRole("link", { name: "Continue" }).click();
+    await expect(page.getByText(`Logged in as ${randomFullName}`)).toBeVisible();
   });
 });
