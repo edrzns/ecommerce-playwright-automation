4cd6e4dd37679a193395a12a818589d7bb3ca567
CERZ <edijs.rozens@bite.lv>
2025-10-27 11:49:56 +0200

    Refactor reporters for CI and local

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,9 +22,9 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-    ['github'],
-    ['html', { open: 'never' }],
+  reporter: process.env.CI ? 'github' : [
+    ['list'],
+    ['html', { open: 'on-failure' }],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
